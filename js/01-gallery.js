f57bc5f9de3712f2e8cf927ce4af5eecf1a5aaf8
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-02-16 21:04:52 +0200

    add 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryElem = document.querySelector(".gallery");
+const previewImg = galleryItems.reduce((acc, item) => {
+  return (acc += `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`);
+}, "");
 
-console.log(galleryItems);
+galleryElem.insertAdjacentHTML("beforeend", previewImg);
+galleryElem.addEventListener("click", openOriginalImage);
+
+function openOriginalImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+}
